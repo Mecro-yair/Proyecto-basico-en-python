536b336c13e10e141034780b685b0c9a73ce39d7
Wesley Laura <wrlaurac@unjbg.edu.pe>
2024-06-14 02:43:29 -0500

    Se agregó entrada y salida de nombre y se actualizó la condicional para que la nota sea en base a 20

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,12 +1,17 @@
+nombre=input("INGRESE EL NOMBRE DEL ALUMNO:\n")
 nota1= float(input("Digite su primera calificación:\n"))
 nota2= float(input("Digite su primera calificación:\n"))
 nota3= float(input("Digite su tercera calificación:\n"))
+
 promedio = (nota1+ nota2 +nota3)/3
-#Se agrego una validacion de notas
-if nota1>=0 and nota2>=0 and nota3>=0:
-    if promedio >=70 :
+#se agregó entrada y salida de nombre del estudiante
+print(nombre)
+#Se agregó una validación de notas
+#Se actualizó la condicional de notas
+if nota1>=0 and nota1<=20 and nota2>=0 and nota2<=20 and nota3>=0 and nota3<=20:
+    if promedio >=10.5 :
         print("Aprobo el curso ")
     else:
         print("Reprobo el curso ")
 else:
-    print("ERROR!_INVALID_NEGATIVE_NUMBER")
+    print("ERROR!_NUMBER")
