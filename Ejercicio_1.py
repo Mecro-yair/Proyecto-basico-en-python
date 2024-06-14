2400a23245059268e4c5b1b5133a50d760db12e0
Yair292 <dluquep@unjbg.edu.pe>
2024-06-14 00:49:05 -0500

    Se añadio el ejercicio de evaluacion de calificacion

---
+++ Ejercicio_1.py
@@ -0,0 +1,11 @@
+nota1= float(input("Digite su primera calificación:\n"))
+nota2= float(input("Digite su primera calificación:\n"))
+nota3= float(input("Digite su tercera calificación:\n"))
+
+promedio = (nota1+ nota2 +nota3)/3
+if promedio >=70 :
+    print("Aprobo el curso ")
+else:
+    print("Reprobo el curso ")
+
+
