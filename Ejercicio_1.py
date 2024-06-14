bc3e97f8f9676102a3114950daecee178fbebb53
Oscar <oticonaf@unjbg.edu.pe>
2024-06-14 01:49:59 -0500

    Se actualizo agregando una validacion de entrada para numeros negativos

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,11 +1,12 @@
 nota1= float(input("Digite su primera calificación:\n"))
 nota2= float(input("Digite su primera calificación:\n"))
 nota3= float(input("Digite su tercera calificación:\n"))
-
 promedio = (nota1+ nota2 +nota3)/3
-if promedio >=70 :
-    print("Aprobo el curso ")
+#Se agrego una validacion de notas
+if nota1>=0 and nota2>=0 and nota3>=0:
+    if promedio >=70 :
+        print("Aprobo el curso ")
+    else:
+        print("Reprobo el curso ")
 else:
-    print("Reprobo el curso ")
-
-
+    print("ERROR!_INVALID_NEGATIVE_NUMBER")
