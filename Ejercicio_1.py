a4f9bd18351ceab6c65d0b651c1b23be37231c32
Yair292 <dluquep@unjbg.edu.pe>
2024-06-14 02:55:41 -0500

    Se cambio de linea la salida del nombre para evitar la escritura en el error

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -4,13 +4,15 @@ nota2= float(input("Digite su primera calificación:\n"))
 nota3= float(input("Digite su tercera calificación:\n"))
 
 promedio = (nota1+ nota2 +nota3)/3
-#se agregó entrada y salida de nombre del estudiante
-print(nombre)
+
 #Se agregó una validación de notas
 #Se actualizó la condicional de notas
 if nota1>=0 and nota1<=20 and nota2>=0 and nota2<=20 and nota3>=0 and nota3<=20:
     if promedio >=10.5 :
+        #se agregó entrada y salida de nombre del estudiante
+        print(nombre)
         print("Aprobo el curso ")
+        
     else:
         print("Reprobo el curso ")
 else:
