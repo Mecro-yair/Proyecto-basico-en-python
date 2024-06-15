be756d370d30912799db1f9094c1990ce47f7dd5
Oscar <oticonaf@unjbg.edu.pe>
2024-06-14 22:38:03 -0500

    Se agrego un bucle para que consulte al usuario si desea continuar

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -8,29 +8,32 @@ def calificacion_en_base20(x,y,z):
     else:
         cumple_base20=False
     return cumple_base20
-
-nombre=input("INGRESE EL NOMBRE DEL ALUMNO:\n")
-nota1= float(input("Digite su primera calificación:\n"))
-nota2= float(input("Digite su primera calificación:\n"))
-nota3= float(input("Digite su tercera calificación:\n"))
-
 """
 Se agregó una validación de notas
 Se actualizó la condicional de notas
 se agregó entrada y salida de nombre del estudiante
 Se optimizo el codigo añadiendo una funciones
+Se agrego un bucle para preguntar si se desea continuar con el programa
 """
-
-#Almaceno funciones
-promedio= Promedio(nota1,nota2,nota3)
-base20 = calificacion_en_base20(nota1,nota2,nota3)
-
-if base20==True:
-    if promedio >=10.5 :
-        print(nombre)
-        print("Aprobo el curso ")
-        
+#Bucle pregunta
+while True:
+    nombre=input("INGRESE EL NOMBRE DEL ALUMNO:\n")
+    nota1= float(input("Digite su primera calificación:\n"))
+    nota2= float(input("Digite su primera calificación:\n"))
+    nota3= float(input("Digite su tercera calificación:\n"))
+    #Almaceno funciones
+    promedio= Promedio(nota1,nota2,nota3)
+    base20=calificacion_en_base20(nota1,nota2,nota3)
+    if base20==True:
+        if promedio >=10.5 :
+            print(nombre)
+            print("Aprobo el curso ")
+        else:
+            print("Reprobo el curso ")
     else:
-        print("Reprobo el curso ")
-else:
-    print("ERROR!_NUMBER")
+        print("ERROR!_NUMBER")
+        exit(0)
+    quest=input("Continuar con otro alumno? si/no:\n")
+    if quest!="si":
+        print("FIN DEL PROGRAMA")
+        exit(0)
