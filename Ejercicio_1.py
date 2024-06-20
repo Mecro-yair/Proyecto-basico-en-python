f782db19b30442f498375544452732a559c8f6bd
Wesley Laura <wrlaurac@unjbg.edu.pe>
2024-06-20 02:06:25 -0500

    Actualizando ejercicio promedios: añadiendo una función para la validacion de nota ponderada, agragando clear_screen

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -21,6 +21,13 @@ def notas_porcentaje(c,p,d,pc,pp,pd):
     nd=d*pd/100
     nf=nc+np+nd
     return nf
+def validación_nota(q,e,n,):
+    if q>=0 and q>=20 and e>=0 and e<=20 and n>=0 and n<=20:
+        Cumple_base20=True
+    else:
+        Cumple_base20=False
+    return Cumple_base20   
+            
 """
 Se agregó una validación de notas
 Se actualizó la condicional de notas
@@ -37,7 +44,7 @@ while True:
     #Validacion de entrada
     try:
         print("MENU DE OPCIONES\n1.Calcular el promedio de 3 notas.\n2.Calcular promedio ponderado.\n3.Salir.")
-        op=int(input("\n*Elija una opcion\n"))
+        op=int(input("\n*Elija una opción\n"))
         if op==1:
             clear_screen()
             #Bucle pregunta
@@ -54,10 +61,10 @@ while True:
                 if base20==True:
                     if promedio>=10.5:
                         print(nombre)
-                        print("Aprobo el curso ")   
+                        print("Aprobó el curso ")   
                     else:
                         print(nombre)
-                        print("Reprobo el curso ")       
+                        print("Reprobó el curso ")       
                 else:
                     print("ERROR!_NUMBER")
                 quest=input("Continuar? si/no:\n")
@@ -75,17 +82,27 @@ while True:
                 nota_P= float(input("Digite su nota de producto:\n"))
                 porcent_D=float(input("Ingrese el Porcentaje de la nota de Desempeño:\n"))
                 nota_D= float(input("Digite su nota de desempeño:\n"))
+                #Agregando validación de entrada de nota
+                valida=validación_nota(nota_C, nota_D, nota_P)
+                if valida==True:
+                    
                 #Almacena 
-                Nota_Final=notas_porcentaje(nota_C,nota_P,nota_D,porcent_C,porcent_P,porcent_D)
-                if  Nota_Final>=10.5 :
-                        print(nombre)
-                        print(str(Nota_Final)+" Aprobo el curso ")   
+                    Nota_Final=notas_porcentaje(nota_C,nota_P,nota_D,porcent_C,porcent_P,porcent_D)
+                    if  Nota_Final>=10.5 :
+                            clear_screen()
+                            print(nombre)
+                            print(str(Nota_Final)+" Aprobó el curso ")   
+                    else:
+                            clear_screen()
+                            print(nombre)
+                            print(str(Nota_Final)+" Reprobó el curso ")
+                    quest=input("Continuar? si/no:\n")
+                    if quest!="si":
+                        break
                 else:
-                        print(nombre)
-                        print(str(Nota_Final)+" Reprobo el curso ")
-                quest=input("Continuar? si/no:\n")
-                if quest!="si":
-                    break
+                    clear_screen()
+                    print("ERROR")
+                    sleep(1.2)    
         if op<1 or op>3:
             clear_screen()
             print("Invalido")
@@ -94,5 +111,5 @@ while True:
             break
     except:
         clear_screen()
-        print("Ingrese una opcion valida")
+        print("Ingrese una opción válida")
         sleep(1)
